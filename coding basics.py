2c1eabac60b6e264f82cfc4cecd6a25c13f81520
12345678dsfgh <finn.bevingtonc@papamoacollege.school.nz>
2026-02-10 09:23:11 +1300

    Add files via upload

---
+++ coding basics.py
@@ -0,0 +1,15 @@
+want_instructions =input("do you want to see the instructions? ").lower()
+
+while True:
+
+    want_instructions =input ("do you want to see the instructions?")
+# check the user says yes / no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("good work sir very proud")
+    elif want_instructions == "no" or want_instructions =="n":
+        print("wrong choice try again")
+
+    else:
+        print("say yes/no or i will find you")
+
+print ("we done")
---
+++ finn python coding/coding basicsv2.py
@@ -0,0 +1,15 @@
+
+while True:
+
+
+want_instructions =input ("do you want to see the instructions?")
+# check the user says yes / no
+if want_instructions == "yes" or want_instructions == "Yes":
+    print("good work sir very proud")
+elif want_instructions == "no":
+    print("wrong choice try again")
+
+else:
+    print("say yes/no or i will find you")
+    continue
+    print ("we done")
